d2351315771ab1f5c24d4b9d98f0e3247b7c3b3d
Fadi Awartani <fadi.alawartani@gmail.com>
2017-04-09 00:18:40 -0400

    Minor

--- AvionicsPCB-C/src/main.c
+++ AvionicsPCB-C/src/main.c
@@ -17,7 +17,7 @@ int main (void) {
 	usart_write_line(&AVR32_USART1, "Hello, this is the AVR UC3 MCU saying hello!\r\n");
 	println_usb_debug("Initialized Board.");
 	
-	//this
+	//this is another change
 	char system_clock[10];
 	sprintf(system_clock, "%d", sysclk_get_main_hz());
 	print_usb_debug("The system clock is: ");
@@ -32,7 +32,7 @@ int i = 0;
 		gpio_tgl_gpio_pin(BLUE_LED_PIN);
 		gpio_tgl_gpio_pin(RED_LED_PIN);
 		print_usb_debug(i);
-		i++
-		delay_ms(500);
+		i++;
+		delay_ms(i % 500);
 	}
 }
