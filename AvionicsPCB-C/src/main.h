7b193b3391c77597c49e585d0b7743b1f7b2bf52
Fadi Awartani <fadi.alawartani@gmail.com>
2017-05-13 23:40:36 -0400

    Added proper I2C communication functions

--- AvionicsPCB-C/src/main.h
+++ AvionicsPCB-C/src/main.h
@@ -7,12 +7,17 @@
 
 #ifndef MAIN_H_
 #define MAIN_H_
+#include <asf.h>
+#include <stdio.h>
 
 extern void initialize_board();
 
+char gen_string[128];
 //End characters for println.
 #define ENDCHARS "\r\n"
 extern void print_usb_debug(char* message);
 extern void println_usb_debug(char* message);
+extern int send_i2c_bytes(uint8_t device_address, uint8_t internal_address, uint8_t *data_bytes, int len);
+extern int read_i2c_bytes(uint8_t device_address, uint8_t internal_address, uint8_t *data_bytes, int len);
 
 #endif /* MAIN_H_ */
