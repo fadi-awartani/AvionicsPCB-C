92bfb0e09ee8565a992c8f783fe472f3048f7760
Fadi Awartani <fadi.alawartani@gmail.com>
2017-06-15 14:56:32 -0400

    update i2c

--- AvionicsPCB-C/src/i2c.c
+++ AvionicsPCB-C/src/i2c.c
@@ -13,17 +13,21 @@ int send_i2c_bytes(uint8_t device_address, uint8_t internal_address, uint8_t *da
 	AVR32_TWI.iadr = internal_address;
 	//AVR32_TWI.cr = 0x1;//Start transfer
 	
+	int count = 0;
 	for(int i = 0; i < len; i++) {
-		int status_reg;
+		volatile int status_reg;
 		
 		AVR32_TWI.thr = *(data_bytes + i);
 		while(!((status_reg = AVR32_TWI.sr) & 0x4)) { //While not TX ready - busy wait loop
+			delay_us(10);
 			if(status_reg & 0x301) //if bits of ARBLST, NACK, or TXCOMP are set.
 				return -1; //error
+			if(count++ > 300)
+				return -2; //timeout
 		} 
 	}
 	
-	int count = 0;
+	count = 0;
 	while(!(AVR32_TWI.sr & 0x1)) { //While not TXCOMP (TX complete) - busy wait loop
 		delay_us(200);
 		if(count++ > 24)
@@ -35,6 +39,7 @@ int send_i2c_bytes(uint8_t device_address, uint8_t internal_address, uint8_t *da
 
 //Based on flowchart from microcontroller datasheet.
 int read_i2c_bytes(uint8_t device_address, uint8_t internal_address, uint8_t *data_bytes, int len) {
+	int count = 0;
 	setup_i2c_masterclock();
 	AVR32_TWI.cr = 0x24;
 	AVR32_TWI.mmr = (device_address << 16) | 0x1100;
@@ -48,6 +53,9 @@ int read_i2c_bytes(uint8_t device_address, uint8_t internal_address, uint8_t *da
 			while(!((status_reg = AVR32_TWI.sr) & 0x2)) { //While not RX ready - busy wait loop
 				if(status_reg & 0x301) //if bits of ARBLST, NACK, or TXCOMP are set.
 					return -1; //error
+				if(count++ > 200) 
+					return -2; //timeout
+				delay_us(10);
 			}
 		
 			*(data_bytes+i) = (uint8_t) AVR32_TWI.rhr;
@@ -61,14 +69,18 @@ int read_i2c_bytes(uint8_t device_address, uint8_t internal_address, uint8_t *da
 	
 	volatile int status_reg;
 		
+	count = 0;
 	while(((status_reg = AVR32_TWI.sr) & 0x2) != 0x2) { //While not RX ready - busy wait loop
 		if(status_reg & 0x301) //if bits of ARBLST, NACK, or TXCOMP are set.
 			return -1; //error
+		if(count++ > 200) 
+			return -2; //timeout
+		delay_us(10);
 	}
 	
 	*(data_bytes+len-1) = (uint8_t) AVR32_TWI.rhr;
 	
-	int count = 0;
+	count = 0;
 	while(!(AVR32_TWI.sr & 0x1)) { //While not TXCOMP (TX complete) - busy wait loop
 		delay_us(200);
 		if(count++ > 24)
